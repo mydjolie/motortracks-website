bfd73c5e1c76183b57d36469fa8aa6fe3cde7968
Mind Sathirasrindh <109655871+mydjolie@users.noreply.github.com>
2023-06-18 23:30:58 +0700

    feat:add coming soon

---
+++ pages/home/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomeSecondary = () => {
+  return <div>HomeSecondary</div>;
+};
+
+export default HomeSecondary;
---
+++ pages/index.js
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import {
+  CircularProgress,
+  CircularProgressLabel,
+  Container,
+  Text,
+  Center,
+  Box,
+  Flex,
+} from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <Container w="100%" h="100%">
+      <Flex
+        h="100vh"
+        flexDirection="column"
+        justifyContent={"center"}
+        alignItems={"center"}
+        gap="4"
+      >
+        <Image
+          alt="motortracks logo"
+          src="/images/home/mt-logo.jpeg"
+          width={400}
+          height={100}
+        ></Image>
+        <Text
+          textAlign="center"
+          fontSize={"60px"}
+          bg="#C9232D"
+          w={"100vw"}
+          color="white"
+          mt={-10}
+        >
+          Coming Soon
+        </Text>
+        <Box>
+          <CircularProgress
+            isIndeterminate
+            value={40}
+            thickness="8px"
+            size="200px"
+            color="#C9232D"
+          >
+            <CircularProgressLabel>10%</CircularProgressLabel>
+          </CircularProgress>
+        </Box>
+      </Flex>
+    </Container>
+  );
+}
